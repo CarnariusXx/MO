d7cda3359a95af9ed59ef782b0f3aba2836381eb
Konrad Mielnik <konrad_mielnik@wp.pl>
2020-04-18 16:01:09 +0200

    Add Test

--- src/zad1804.jl
+++ src/zad1804.jl
@@ -1,5 +1,6 @@
 module zad1804
 
+"Function of one variable"
 my_f(x) = x^4 - 10x + 2
 
 export my_f
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using zad1804, Test
+
+@test my_f(1) == -7
+@test my_f(0) == 2
+@test my_f(1.5) == -7.9375
